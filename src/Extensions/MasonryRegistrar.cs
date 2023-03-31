f2835a03acc452ce3b554b37c05b4b092a564f6d
Jake Soenneker <jake@soenneker.com>
2023-03-30 20:44:15 -0500

    Misc

--- src/Extensions/MasonryRegistrar.cs
+++ src/Extensions/MasonryRegistrar.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Soenneker.Blazor.Masonry.Abstract;
 
 namespace Soenneker.Blazor.Masonry.Extensions;
 
+/// <summary>
+/// A small Blazor interop library that integrates Masonry (https://masonry.desandro.com), the cascading grid layout library
+/// </summary>
 public static class MasonryRegistrar
 {
     /// <summary>
-    /// Shorthand for <code>services.AddScoped</code>
+    /// Shorthand for <code>services.TryAddScoped</code>
     /// </summary>
     public static void AddMasonry(this IServiceCollection services)
     {
-        services.AddScoped<IMasonryInterop, MasonryInterop>();
+        services.TryAddScoped<IMasonryInterop, MasonryInterop>();
     }
 }
