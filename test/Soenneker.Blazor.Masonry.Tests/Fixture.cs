9808331b81ddf740643e5def050511471e7f236a
Jake Soenneker <jake@soenneker.com>
2024-09-19 10:47:20 -0500

    misc

--- test/Soenneker.Blazor.Masonry.Tests/Fixture.cs
+++ test/Soenneker.Blazor.Masonry.Tests/Fixture.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 using Serilog;
 using Soenneker.Blazor.Masonry.Registrars;
 using Soenneker.Fixtures.Unit;
@@ -22,7 +23,7 @@ public class Fixture : UnitFixture
 
         IConfiguration config = TestUtil.BuildConfig();
         services.AddSingleton(config);
-
+        
         services.AddMasonry();
     }
 }
