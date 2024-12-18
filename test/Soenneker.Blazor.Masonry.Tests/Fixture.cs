51b2ea6525a869777ec990c4948280478e1af96e
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:34:43 +0000

    xUnit v3

--- test/Soenneker.Blazor.Masonry.Tests/Fixture.cs
+++ test/Soenneker.Blazor.Masonry.Tests/Fixture.cs
@@ -10,7 +10,7 @@ namespace Soenneker.Blazor.Masonry.Tests;
 
 public class Fixture : UnitFixture
 {
-    public override System.Threading.Tasks.Task InitializeAsync()
+    public override System.Threading.Tasks.ValueTask InitializeAsync()
     {
         SetupIoC(Services);
 
--- test/Soenneker.Blazor.Masonry.Tests/MasonryInteropTests.cs
+++ test/Soenneker.Blazor.Masonry.Tests/MasonryInteropTests.cs
@@ -1,7 +1,7 @@
 using Soenneker.Blazor.Masonry.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
-using Xunit.Abstractions;
+
 
 namespace Soenneker.Blazor.Masonry.Tests;
 
