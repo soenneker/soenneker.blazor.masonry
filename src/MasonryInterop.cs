3b6a6734a7fa8c623df75cedf1b6e084b56224f5
Jake Soenneker <jake@soenneker.com>
2025-12-21 09:14:21 -0600

    Deps

--- src/MasonryInterop.cs
+++ src/MasonryInterop.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.JSInterop;
 using Soenneker.Blazor.Masonry.Abstract;
 using Soenneker.Blazor.Utils.ResourceLoader.Abstract;
-using Soenneker.Utils.AsyncSingleton;
+using Soenneker.Asyncs.Initializers;
 
 namespace Soenneker.Blazor.Masonry;
 
@@ -13,7 +13,7 @@ public sealed class MasonryInterop : IMasonryInterop
     private readonly IJSRuntime _jsRuntime;
     private readonly IResourceLoader _resourceLoader;
 
-    private readonly AsyncSingleton _scriptInitializer;
+    private readonly AsyncInitializer<bool> _scriptInitializer;
 
     private const string _modulePath = "Soenneker.Blazor.Masonry/js/masonryinterop.js";
     private const string _moduleName = "MasonryInterop";
@@ -23,48 +23,39 @@ public sealed class MasonryInterop : IMasonryInterop
         _jsRuntime = jSRuntime;
         _resourceLoader = resourceLoader;
 
-        _scriptInitializer = new AsyncSingleton(async (token, obj) =>
+        _scriptInitializer = new AsyncInitializer<bool>(async (useCdn, token) =>
         {
-            var useCdn = true;
-
-            if (obj.Length > 0)
-                useCdn = (bool)obj[0];
-
             if (useCdn)
             {
                 await _resourceLoader.LoadScriptAndWaitForVariable("https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js", "Masonry",
-                                         "sha256-Nn1q/fx0H7SNLZMQ5Hw5JLaTRZp0yILA/FRexe19VdI=", cancellationToken: token)
-                                     ;
+                    "sha256-Nn1q/fx0H7SNLZMQ5Hw5JLaTRZp0yILA/FRexe19VdI=", cancellationToken: token);
             }
             else
             {
-                await _resourceLoader
-                      .LoadScriptAndWaitForVariable("_content/Soenneker.Blazor.Masonry/js/masonry.pkgd.min.js", "Masonry", cancellationToken: token)
-                      ;
+                await _resourceLoader.LoadScriptAndWaitForVariable("_content/Soenneker.Blazor.Masonry/js/masonry.pkgd.min.js", "Masonry",
+                    cancellationToken: token);
             }
 
             await _resourceLoader.ImportModuleAndWaitUntilAvailable(_modulePath, _moduleName, 100, token);
-
-            return new object();
         });
     }
 
     public ValueTask Warmup(bool useCdn = true, CancellationToken cancellationToken = default)
     {
-        return _scriptInitializer.Init(cancellationToken, useCdn);
+        return _scriptInitializer.Init(useCdn, cancellationToken);
     }
 
-    public async ValueTask Init(string elementId, string? containerSelector = null, string itemSelector = ".masonry-item", string? columnWidthSelector = null, bool percentPosition = true,
-        float transitionDurationSecs = .2F, bool useCdn = true, CancellationToken cancellationToken = default)
+    public async ValueTask Init(string elementId, string? containerSelector = null, string itemSelector = ".masonry-item", string? columnWidthSelector = null,
+        bool percentPosition = true, float transitionDurationSecs = .2F, bool useCdn = true, CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken, useCdn);
+        await _scriptInitializer.Init(useCdn, cancellationToken);
 
         containerSelector ??= $"#{elementId}";
 
         var transitionDurationStr = $"{transitionDurationSecs}s";
 
-        await _jsRuntime.InvokeVoidAsync($"{_moduleName}.init", cancellationToken, elementId, containerSelector, itemSelector, columnWidthSelector, percentPosition, transitionDurationStr)
-                        ;
+        await _jsRuntime.InvokeVoidAsync($"{_moduleName}.init", cancellationToken, elementId, containerSelector, itemSelector, columnWidthSelector,
+            percentPosition, transitionDurationStr);
     }
 
     public ValueTask CreateObserver(string elementId, CancellationToken cancellationToken = default)
@@ -74,14 +65,14 @@ public sealed class MasonryInterop : IMasonryInterop
 
     public async ValueTask Layout(string elementId, CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken);
+        await _scriptInitializer.Init(true, cancellationToken);
 
         await _jsRuntime.InvokeVoidAsync($"{_moduleName}.layout", cancellationToken, elementId);
     }
 
     public ValueTask Destroy(string elementId, CancellationToken cancellationToken = default)
     {
-         return _jsRuntime.InvokeVoidAsync($"{_moduleName}.destroy", cancellationToken, elementId);
+        return _jsRuntime.InvokeVoidAsync($"{_moduleName}.destroy", cancellationToken, elementId);
     }
 
     public async ValueTask DisposeAsync()
