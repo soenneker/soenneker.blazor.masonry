082cd52032001801c8a323a5b0d2acf996e98c01
Jake Soenneker <jake@soenneker.com>
2026-04-30 19:51:18 -0500

    -> lepton

--- src/Soenneker.Blazor.Masonry/Abstract/IMasonry.cs
+++ src/Soenneker.Blazor.Masonry/Abstract/IMasonry.cs
@@ -1,13 +1,13 @@
 using System.Threading;
 using System.Threading.Tasks;
-using Soenneker.Quark;
+using Soenneker.Lepton.Suite.Abstract;
 
 namespace Soenneker.Blazor.Masonry.Abstract;
 
 /// <summary>
 /// Defines the contract for a Masonry Blazor component interop wrapper.
 /// </summary>
-public interface IMasonry : ICoreElement
+public interface IMasonry : ILeptonIdentifiableContentElement
 {
     /// <summary>
     /// Gets or sets a value indicating whether the component should automatically initialize Masonry on first render.
